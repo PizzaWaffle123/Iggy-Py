5ea0317a79f823b61537cf6d3ba3677f9431224c
evo <thetrueevo@gmail.com>
2021-06-10 08:48:56 -0400

    Begins work on streamlining and optimizing the main file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import cgh
+import polls
 import welcome
 import verify
 from discord.ext import commands
+import commands as iggycommands
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix='$')
 
@@ -13,7 +15,9 @@ async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
     cgh.setup(bot.guilds[0])
     bot.load_extension('commands')
-    await cgh.get_channel_obj_by_name("server_work").send(embed=verify.get_embed_by_name("changelog", "TestData"))
+    verify.my_bot = bot
+    await polls.refresh_from_file()
+    # await cgh.get_channel_obj_by_name("server_work").send(embed=verify.get_embed_by_name("changelog", "TestData"))
 
 
 @bot.event
@@ -40,14 +44,6 @@ async def on_user_update(before, after):
 
 
 @bot.event
-async def on_reaction_add(reaction, user):
-    # This event happens whenever a reaction gets added to a message the bot can see.
-    if user == bot.user:
-        return
-    await verify.new_input(user, None, reaction, reaction.message.channel, reaction.message)
-
-
-@bot.event
 async def on_member_update(before, after):
     # This event happens whenever something server-specific happens to a user.
     # We use this to track role changes.
@@ -58,6 +54,17 @@ async def on_member_update(before, after):
         if role.id == cgh.get_role_id_by_name("crusader"):
             cgh.welcome_message(after)
             break
+
+
+@bot.event
+async def on_interaction(interaction):
+    print("Heard an interaction! (%s#%s)" % (interaction.user.name, interaction.user.discriminator))
+    if "name" in interaction.data:
+        print("I think it's a command!")
+        await iggycommands.handle(interaction, bot)
+    else:
+        print("Could be a poll response!")
+        await polls.handle_interaction(interaction)
 
 
 @bot.event
