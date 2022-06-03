efa965637e9c027eed62da37814a6c60f72ad604
evo <thetrueevo@gmail.com>
2022-06-03 15:56:44 -0400

    Adds code relevant to registering (and later handling) commands.

---
+++ commands.py
@@ -0,0 +1,25 @@
+import json
+
+import requests as requests
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    # Transmits commands to Discord.
+    print("Sending commands...")
+    with open("commands.json", "r") as command_file:
+        data = command_file.read()
+        data = json.loads(data)
+
+    app_id = os.getenv("app_id")
+    api_url = f"https://discord.com/api/v10/applications/{app_id}/commands"
+
+    token = os.getenv("token")
+    headers = {
+        "Authorization": f"Bot {token}"
+    }
+
+    r = requests.post(url=api_url, headers=headers, json=data)
+    print(r.content)
