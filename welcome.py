386b1a7db5c5a6d40e8c99efbf0dafabd032e1ef
evo <thetrueevo@gmail.com>
2022-06-05 00:10:51 -0400

    Adds support for user-defined introductions in the welcome embed

--- welcome.py
+++ welcome.py
@@ -7,7 +7,7 @@ def ordinal_num(n):
     return str(n) + {1: 'st', 2: 'nd', 3: 'rd'}.get(4 if 10 <= n % 100 < 20 else n % 10, "th")
 
 
-def get_welcome_embed(member):
+def get_welcome_embed(member, intro=None):
     # Returns an embed which can then be sent in a message.
     # Working with embeds is really fun and should be self-explanatory.
     embed = discord.Embed()
@@ -23,12 +23,22 @@ def get_welcome_embed(member):
     title = title.format(count=count, ordinal=ordinal, name=name)
     body = body.format(count=count, ordinal=ordinal, name=name)
 
-    body += "\n\n**Profile: {}**".format(member.mention)
+    embed.description = body
+
+    # body += "\n\n**Profile: {}**".format(member.mention)
 
     embed.title = title
-    full_name = member.name + "#" + member.discriminator
-    embed.add_field(name=full_name, value=body, inline=False)
+    # full_name = member.name + "#" + member.discriminator
+    # embed.add_field(name=full_name, value=body, inline=False)
     embed.set_thumbnail(url=member.avatar.url)
+
+    # footer = "**Profile: {}**".format(member.mention)
+    # embed.set_footer(text=footer)
+
+    if intro is not None:
+        embed.add_field(name="Introduction", value=intro, inline=True)
+
+    embed.add_field(name="Profile", value=member.mention, inline=True)
 
     return embed
 
